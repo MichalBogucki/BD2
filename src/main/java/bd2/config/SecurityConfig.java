2ed4cc7c7200aa855ffc9b7d0450711c0de9d0e5
Griczendo <kamil0930@gmail.com>
2018-12-27 12:28:53 +0100

    Fixed pom.xml dependecies, added validator

--- src/main/java/bd2/config/SecurityConfig.java
+++ src/main/java/bd2/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import bd2.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -44,6 +45,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(authenticationService).passwordEncoder(passwordEncoder());
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+		auth.setUserDetailsService(authenticationService);
+		auth.setPasswordEncoder(passwordEncoder());
+		return auth;
 	}
 
 	@Bean
---
+++ src/main/java/bd2/validation/ValidEmail.java
@@ -0,0 +1,17 @@
+package bd2.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Constraint(validatedBy = EmailValidator.class)
+@Target({ ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface ValidEmail {
+	String message() default "Invalid email";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+}
