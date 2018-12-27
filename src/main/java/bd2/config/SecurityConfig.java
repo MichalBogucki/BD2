a92e775ba81ee730ec0a7b3554e346d5e3e1fdd2
Griczendo <kamil0930@gmail.com>
2018-12-27 12:15:54 +0100

    Removed redundant annotations

--- src/main/java/bd2/config/SecurityConfig.java
+++ src/main/java/bd2/config/SecurityConfig.java
@@ -3,48 +3,20 @@ package bd2.config;
 import bd2.service.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@ComponentScan("bd2")
-@EnableTransactionManagement
-@PropertySource({ "classpath:persistence-mssql.properties" })
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	AuthenticationService authenticationService;
-
-	@Autowired
-	private Environment env;
-
-	@Bean
-	public DataSource myDataSource() {
-
-		DriverManagerDataSource myDataSource = new DriverManagerDataSource();
-
-		myDataSource.setDriverClassName(env.getProperty("jdbc.driver"));
-		myDataSource.setUrl(env.getProperty("jdbc.url"));
-		myDataSource.setUsername(env.getProperty("jdbc.user"));
-		myDataSource.setPassword(env.getProperty("jdbc.password"));
-
-		return myDataSource;
-	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
